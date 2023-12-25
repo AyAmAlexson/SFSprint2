e997294ec0d4920301c1405f650a806026ce9cb3
AyAmAlexson <125996865+AyAmAlexson@users.noreply.github.com>
2023-12-25 15:24:40 +0400

    Update wsgi.py

--- SFSprint2/SFSprint2/wsgi.py
+++ SFSprint2/SFSprint2/wsgi.py
@@ -10,5 +10,6 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 import os
 from SFSprint2.settings import get_wsgi_application
 
+print("Python Path:", sys.path)  # Add this line to print the Python path
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SFSprint2.settings')
 application = get_wsgi_application()
