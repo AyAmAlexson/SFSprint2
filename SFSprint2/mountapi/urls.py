894bfbe0fee427aeb8eff47e65dbea36934b28d4
AyAmAlexson <125996865+AyAmAlexson@users.noreply.github.com>
2023-12-26 14:48:02 +0400

    Update urls.py

--- SFSprint2/mountapi/urls.py
+++ SFSprint2/mountapi/urls.py
@@ -4,8 +4,8 @@ from .views import *
 urlpatterns = [
     path('submitData/', SubmitDataView.as_view(), name='submit_data'),
     path('createUser/', AddNewUser.as_view(), name='create_user'),
-    path('getData/<int:id>/', GetMountainPassView.as_view(), name='get_mountain_pass'),
-    path('editData/<int:id>/', EditMountainPassView.as_view(), name='edit_mountain_pass'),
+    path('getData/<int:pk>/', GetMountainPassView.as_view(), name='get_mountain_pass'),
+    path('editData/<int:pk>/', EditMountainPassView.as_view(), name='edit_mountain_pass'),
     path('userMountainPassList/', GetUserMountainPassListView.as_view(), name='get_user_mountain_pass_list'),
     # ...
 ]
