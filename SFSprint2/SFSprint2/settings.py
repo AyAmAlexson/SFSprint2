c68f4e6e432433e464c8dfee83bf23cfdf90ff50
AyAmAlexson <125996865+AyAmAlexson@users.noreply.github.com>
2023-12-28 17:02:24 +0400

    Update settings.py

--- SFSprint2/SFSprint2/settings.py
+++ SFSprint2/SFSprint2/settings.py
@@ -90,7 +90,7 @@ USE_TZ = True
 
 
 STATIC_URL = 'static/'
-STATIC_ROOT = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
