f90b64e6dc4c4709475d1e7841523594a92ca399
AyAmAlexson <125996865+AyAmAlexson@users.noreply.github.com>
2023-12-28 15:41:15 +0400

    Update views.py

--- SFSprint2/mountapi/views.py
+++ SFSprint2/mountapi/views.py
@@ -28,7 +28,7 @@ class AddNewUser(APIView):
             data = json.loads(request.body.decode('utf-8'))
             result = UserManager.submit_data(data)
             return Response(result, status=status.HTTP_200_OK)
-        
+
         except json.JSONDecodeError:
             return Response({'status': 400, 'message': 'Input Data Error', 'id': None}, status=status.HTTP_400_BAD_REQUEST)
 
@@ -39,7 +39,7 @@ class GetMountainPassView(APIView):
         context = {'request': request}
         serializer = MountainPassSerializer(mountain_pass, context=context)
         return Response(serializer.data, status=status.HTTP_200_OK)
-    
+
 
 class EditMountainPassView(APIView):
     def patch(self, request, pk, *args, **kwargs):
@@ -53,12 +53,12 @@ class EditMountainPassView(APIView):
             if mountain_pass.status != 'new':
                 return Response({'status': 0, 'message': 'Нельзя редактировать запись в статусе, отличном от "new".'}, status=status.HTTP_400_BAD_REQUEST)
 
-            mountain_pass.beauty_title = data['beauty_title']
-            mountain_pass.title = data['title']
-            mountain_pass.other_titles = data['other_titles']
-            mountain_pass.connect = data['connect']
-            mountain_pass.coord = data['coords']
-            mountain_pass.level = data['level']
+            mountain_pass.beauty_title = data['beauty_title'] if 'beauty_title' in data else mountain_pass.beauty_title
+            mountain_pass.title = data['title'] if 'title' in data else mountain_pass.title
+            mountain_pass.other_titles = data['other_titles'] if 'other_titles' in data else mountain_pass.other_titles
+            mountain_pass.connect = data['connect'] if 'connect' in data else mountain_pass.connect
+            mountain_pass.coord = data['coords'] if 'coords' in data else mountain_pass.coords
+            mountain_pass.level = data['level'] if 'level' in data else mountain_pass.level
 
 
             mountain_pass.save()
