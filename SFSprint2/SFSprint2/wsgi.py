0e18ffcd8edd0759d68e6fb8ae90683c0b9bf4b5
AyAmAlexson <125996865+AyAmAlexson@users.noreply.github.com>
2023-12-29 13:12:28 +0400

    Update wsgi.py

--- SFSprint2/SFSprint2/wsgi.py
+++ SFSprint2/SFSprint2/wsgi.py
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
-from SFSprint2.settings import get_wsgi_application
+from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SFSprint2.settings')
 application = get_wsgi_application()
