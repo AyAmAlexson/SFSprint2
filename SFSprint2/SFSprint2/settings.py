8c2817658797b9ff56db3eb25633a4b312f2e72c
AyAmAlexson <125996865+AyAmAlexson@users.noreply.github.com>
2023-12-29 12:23:00 +0400

    added static heroku support

--- SFSprint2/SFSprint2/settings.py
+++ SFSprint2/SFSprint2/settings.py
@@ -1,7 +1,7 @@
 import environ
 from pathlib import Path
 import os
-from django.core.wsgi import get_wsgi_application
+import django_heroku
 import dj_database_url
 
 env = environ.Env()
@@ -38,6 +38,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'SFSprint2.urls'
@@ -92,8 +93,15 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+django_heroku.settings(locals())
